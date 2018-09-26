754f350e661c0cae41f71ac2fb758ff2f3817e47
mengCareers <meng.careers@gmail.com>
2018-09-25 21:17:47 -0700

    C

--- src/DynamicProgramming/LongestCommonSubsequence.java
+++ src/DynamicProgramming/LongestCommonSubsequence.java
@@ -8,13 +8,27 @@ package DynamicProgramming;
  *
  * @author xinrong
  */
-public class LCS {
+public class LongestCommonSubsequence {
 
     public static void main(String[] args) {
-        System.out.println(lcs("abcd", "bede"));
+        System.out.println(lenLCSubsequenceBottomup("abcd", "bede"));
     }
 
-    public static int lcs(String s, String t) {
+    public static int lenLCSubsequenceBottomup(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (s1.charAt(i - 1) != s2.charAt(j - 1)) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+
+    public static int lenLCSubsequence(String s, String t) {
         int[][] dp = new int[s.length() + 1][t.length() + 1];
         for (int[] tmp : dp) {
             for (int i = 0; i < tmp.length; i++) {
